a018d1015596f7f5ac611bfa4905d97e278a59cd
DenianHoerlle <deniancloss@gmail.com>
2025-11-29 14:42:24 -0300

    style: :art: run prettier through project

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -16,19 +16,17 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div >
-        <button onClick={() => setCount((count) => count + 1)}>
+      <div>
+        <button onClick={() => setCount(count => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p>
-        Click on the Vite and React logos to learn more
-      </p>
+      <p>Click on the Vite and React logos to learn more</p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
