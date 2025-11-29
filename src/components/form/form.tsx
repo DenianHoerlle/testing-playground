78353566274f23d5d5ddd8f1c5e922fc91bfe64f
DenianHoerlle <deniancloss@gmail.com>
2025-11-29 16:05:02 -0300

    chore: :building_construction: created project folder structure

---
+++ src/components/form/form.tsx
@@ -0,0 +1,13 @@
+import Button from "components/button/button";
+import Input from "components/input/input";
+
+const Form = () => {
+  return (
+    <form>
+      <Input />
+      <Button />
+    </form>
+  );
+};
+
+export default Form;
